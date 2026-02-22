b46ba509d57d1392b1df0e913011f27248dd75e6
yoshue <52592749+yoshue@users.noreply.github.com>
2026-02-22 02:13:55 -0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/tuapp/squidplayer/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.tuapp.squidplayer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme(colorScheme = darkColorScheme()) {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Text(text = "Marco de App 2026 verificado")
+                }
+            }
+        }
+    }
+}
